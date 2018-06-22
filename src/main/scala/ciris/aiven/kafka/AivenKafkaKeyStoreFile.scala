8b35f26d746a4610d9075b25d5106b04b301fefa
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-06-22 11:44:49 +0100

    Change to require Sync[F]

--- src/main/scala/ciris/aiven/kafka/AivenKafkaKeyStoreFile.scala
+++ src/main/scala/ciris/aiven/kafka/AivenKafkaKeyStoreFile.scala
@@ -1,18 +1,22 @@
 package ciris.aiven.kafka
 
 import better.files.File
-
-import scala.util.Try
+import ciris.api.Sync
+import ciris.api.syntax._
 
 sealed abstract case class AivenKafkaKeyStoreFile(value: File)
 
 object AivenKafkaKeyStoreFile {
-  def newTemporary(): Try[AivenKafkaKeyStoreFile] =
-    Try(new AivenKafkaKeyStoreFile({
-      val file = File
-        .newTemporaryFile("client.keystore-", ".p12")
-        .delete(swallowIOExceptions = true)
-      file.toJava.deleteOnExit()
-      file
-    }) {})
+  def newTemporary[F[_]](implicit F: Sync[F]): F[AivenKafkaKeyStoreFile] =
+    F.suspend {
+      F.pure {
+        new AivenKafkaKeyStoreFile({
+          val file = File
+            .newTemporaryFile("client.keystore-", ".p12")
+            .delete(swallowIOExceptions = true)
+          file.toJava.deleteOnExit()
+          file
+        }) {}
+      }
+    }
 }
--- src/main/scala/ciris/aiven/kafka/AivenKafkaTrustStoreFile.scala
+++ src/main/scala/ciris/aiven/kafka/AivenKafkaTrustStoreFile.scala
@@ -1,18 +1,22 @@
 package ciris.aiven.kafka
 
 import better.files.File
-
-import scala.util.Try
+import ciris.api.Sync
+import ciris.api.syntax._
 
 sealed abstract case class AivenKafkaTrustStoreFile(value: File)
 
 object AivenKafkaTrustStoreFile {
-  def newTemporary(): Try[AivenKafkaTrustStoreFile] =
-    Try(new AivenKafkaTrustStoreFile({
-      val file = File
-        .newTemporaryFile("client.truststore-", ".jks")
-        .delete(swallowIOExceptions = true)
-      file.toJava.deleteOnExit()
-      file
-    }) {})
+  def newTemporary[F[_]](implicit F: Sync[F]): F[AivenKafkaTrustStoreFile] =
+    F.suspend {
+      F.pure {
+        new AivenKafkaTrustStoreFile({
+          val file = File
+            .newTemporaryFile("client.truststore-", ".jks")
+            .delete(swallowIOExceptions = true)
+          file.toJava.deleteOnExit()
+          file
+        }) {}
+      }
+    }
 }
