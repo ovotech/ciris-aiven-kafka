a975897ae2af868eb2b0af3a1e2853cb397c6ae5
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-09-07 16:42:59 +0100

    Change to use sha1 short hash for key and certificates

--- src/main/scala/ciris/aiven/kafka/AivenKafkaClientCertificate.scala
+++ src/main/scala/ciris/aiven/kafka/AivenKafkaClientCertificate.scala
@@ -3,17 +3,19 @@ package ciris.aiven.kafka
 import java.io.ByteArrayInputStream
 import java.security.cert.{Certificate, CertificateFactory}
 
+import ciris.internal.digest.sha1Hex
+
 import scala.util.Try
 
-final class AivenKafkaClientCertificate private (val value: Certificate) {
-  override def toString: String =
-    "AivenKafkaClientCertificate(***)"
-}
+sealed abstract case class AivenKafkaClientCertificate(value: Certificate)
 
 object AivenKafkaClientCertificate {
   def fromString(certificate: String): Try[AivenKafkaClientCertificate] =
     Try(new AivenKafkaClientCertificate({
       val bais = new ByteArrayInputStream(certificate.getBytes("UTF-8"))
       CertificateFactory.getInstance("X.509").generateCertificate(bais)
-    }))
+    }) {
+      override def toString: String =
+        s"AivenKafkaClientCertificate(${sha1Hex(certificate).take(7)})"
+    })
 }
--- src/main/scala/ciris/aiven/kafka/AivenKafkaServiceCertificate.scala
+++ src/main/scala/ciris/aiven/kafka/AivenKafkaServiceCertificate.scala
@@ -3,17 +3,19 @@ package ciris.aiven.kafka
 import java.io.ByteArrayInputStream
 import java.security.cert.{Certificate, CertificateFactory}
 
+import ciris.internal.digest.sha1Hex
+
 import scala.util.Try
 
-final class AivenKafkaServiceCertificate private (val value: Certificate) {
-  override def toString: String =
-    "AivenKafkaServiceCertificate(***)"
-}
+sealed abstract case class AivenKafkaServiceCertificate(value: Certificate)
 
 object AivenKafkaServiceCertificate {
   def fromString(certificate: String): Try[AivenKafkaServiceCertificate] =
     Try(new AivenKafkaServiceCertificate({
       val bais = new ByteArrayInputStream(certificate.getBytes("UTF-8"))
       CertificateFactory.getInstance("X.509").generateCertificate(bais)
-    }))
+    }) {
+      override def toString: String =
+        s"AivenKafkaServiceCertificate(${sha1Hex(certificate).take(7)})"
+    })
 }
