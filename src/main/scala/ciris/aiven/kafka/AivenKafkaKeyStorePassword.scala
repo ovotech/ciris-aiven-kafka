1e57057724664449a44854f1124f78cdfd1ae688
Viktor Lövgren <viktor.lovgren@ovoenergy.com>
2018-10-28 22:14:05 +0000

    Change to use hash for key and trust store passwords

--- src/main/scala/ciris/aiven/kafka/AivenKafkaKeyStorePassword.scala
+++ src/main/scala/ciris/aiven/kafka/AivenKafkaKeyStorePassword.scala
@@ -3,13 +3,14 @@ package ciris.aiven.kafka
 import java.util.UUID
 
 import ciris.api.Sync
+import ciris.internal.digest.sha1Hex
 
-final class AivenKafkaKeyStorePassword private (val value: String) {
-  override def toString: String =
-    "AivenKafkaKeyStorePassword(***)"
-}
+sealed abstract case class AivenKafkaKeyStorePassword(value: String)
 
 object AivenKafkaKeyStorePassword {
   def newTemporary[F[_]](implicit F: Sync[F]): F[AivenKafkaKeyStorePassword] =
-    F.suspend(F.pure(new AivenKafkaKeyStorePassword(UUID.randomUUID().toString)))
+    F.suspend(F.pure(new AivenKafkaKeyStorePassword(UUID.randomUUID().toString) {
+      override def toString: String =
+        s"AivenKafkaKeyStorePassword(${sha1Hex(value).take(7)})"
+    }))
 }
--- src/main/scala/ciris/aiven/kafka/AivenKafkaTrustStorePassword.scala
+++ src/main/scala/ciris/aiven/kafka/AivenKafkaTrustStorePassword.scala
@@ -3,13 +3,14 @@ package ciris.aiven.kafka
 import java.util.UUID
 
 import ciris.api.Sync
+import ciris.internal.digest.sha1Hex
 
-final class AivenKafkaTrustStorePassword private (val value: String) {
-  override def toString: String =
-    "AivenKafkaTrustStorePassword(***)"
-}
+sealed abstract case class AivenKafkaTrustStorePassword(value: String)
 
 object AivenKafkaTrustStorePassword {
   def newTemporary[F[_]](implicit F: Sync[F]): F[AivenKafkaTrustStorePassword] =
-    F.suspend(F.pure(new AivenKafkaTrustStorePassword(UUID.randomUUID().toString)))
+    F.suspend(F.pure(new AivenKafkaTrustStorePassword(UUID.randomUUID().toString) {
+      override def toString: String =
+        s"AivenKafkaTrustStorePassword(${sha1Hex(value).take(7)})"
+    }))
 }
